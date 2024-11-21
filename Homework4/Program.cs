3bdb8e60532ad92687bd5b01927d255a88333438
MrHumidum <salavatcool5@gmail.com>
2024-11-21 22:04:24 +0300

    done

---
+++ Homework4/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {/// <summary>
+     /// Генерирует массив из 20 случайных чисел и позволяет пользователю ввести два числа для обмена местами в массиве.
+     /// Если оба числа присутствуют в массиве, их значения меняются местами, и результат выводится на экран.
+     /// </summary>
+        static void Ex1()
+        {
+            Random rnd = new Random();
+            int[] list = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                list[i] = rnd.Next();
+            }
+            foreach (int number in list)
+            {
+                Console.WriteLine(number);
+            }
+            Console.Write("Введите 2 цифры, которые вы хотите поменять местами: ");
+            if ((int.TryParse(Console.ReadLine(), out int number1) && int.TryParse(Console.ReadLine(), out int number2)))
+            {
+                Console.WriteLine();
+                if (list.Contains(number1) && list.Contains(number2))
+                {
+                    for (int i = 0; i < 20; i++)
+                    {
+                        if (list[i] == number1)
+                        {
+                            list[i] = number2;
+                        }
+                        else if (list[i] == number2)
+                        {
+                            list[i] = number1;
+                        }
+                    }
+                    foreach (int number in list)
+                    {
+                        Console.WriteLine(number);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Нужно, чтобы оба числа были из массива");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода данных");
+            }
+        }
+        /// <summary>
+        /// Вычисляет сумму элементов массива, произведение всех элементов и среднее арифметическое элементов массива.
+        /// Результаты возвращаются через параметры `ref` (произведение) и `out` (среднее арифметическое).
+        /// </summary>
+        /// <param name="numbers">Массив чисел.</param>
+        /// <param name="product">Произведение элементов массива (выходной параметр, передается по ссылке).</param>
+        /// <param name="average">Среднее арифметическое элементов массива (выходной параметр, передается по ссылке).</param>
+        /// <returns>Сумма элементов массива.</returns>
+
+        static double Ex2(double[] numbers, ref double product, out double average)
+        {
+            double sum = 0;
+            product = 1;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+                product *= number;
+            }
+
+            average = numbers.Length > 0 ? sum / numbers.Length : 0;
+
+            return sum;
+        }
+        /// <summary>
+        /// Запрашивает у пользователя ввод числа. Если введённое число от 0 до 9, рисует его в виде символов `#`.
+        /// Если введено некорректное число или слово "exit" или "закрыть", программа завершает работу.
+        /// В случае ввода нечислового значения выбрасывает исключение.
+        /// </summary>
+        static void Ex3()
+        {
+            try
+            {
+                Console.Write("Введите число (или 'exit'/'закрыть' для завершения): ");
+                string input = Console.ReadLine();
+
+                if (input.Equals("exit", StringComparison.OrdinalIgnoreCase) ||
+                    input.Equals("закрыть", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Программа завершена.");
+                    return;
+                }
+
+                if (!int.TryParse(input, out int number))
+                {
+                    throw new FormatException("Введено не число.");
+                }
+
+                if (number >= 0 && number <= 9)
+                {
+                    DrawNumber(number);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка: число должно быть от 0 до 9!");
+                    Thread.Sleep(3000);
+                    Console.ResetColor();
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                Thread.Sleep(3000);
+                Console.ResetColor();
+            }
+
+            Ex3();
+        }
+
+        /// <summary>
+        /// Отображает число в виде символа `#` в формате цифры от 0 до 9, используя многократные строки.
+        /// </summary>
+        /// <param name="number">Число от 0 до 9.</param>
+        static void DrawNumber(int number)
+        {
+            string[] digits = new string[]
+            {
+                "###\n# #\n# #\n# #\n###", // 0
+                "  #\n  #\n  #\n  #\n  #", // 1
+                "###\n  #\n###\n#  \n###", // 2
+                "###\n  #\n###\n  #\n###", // 3
+                "# #\n# #\n###\n  #\n  #", // 4
+                "###\n#  \n###\n  #\n###", // 5
+                "###\n#  \n###\n# #\n###", // 6
+                "###\n  #\n  #\n  #\n  #", // 7
+                "###\n# #\n###\n# #\n###", // 8
+                "###\n# #\n###\n  #\n###"  // 9
+            };
+
+            Console.WriteLine(digits[number]);
+        }
+
+        /// <summary>
+        /// Перечисление, описывающее уровни ворчливости деда.
+        /// </summary>
+        enum GrumpinessLevel
+        {
+            Low,
+            Medium,
+            High,
+            Extreme
+        }
+        /// <summary>
+        /// Структура, представляющая "Деда" с именем, уровнем ворчливости, массивом фраз и количеством синяков.
+        /// Включает метод для подсчета фингалов, если дед использует матерные слова из переданного списка.
+        /// </summary>
+        struct Ded
+        {
+            public string Name;
+            public GrumpinessLevel Grumpiness;
+            public string[] Phrases;
+            public int Bruises;
+
+            public Ded(string name, GrumpinessLevel grumpiness, string[] phrases)
+            {
+                Name = name;
+                Grumpiness = grumpiness;
+                Phrases = phrases;
+                Bruises = 0;
+            }
+            /// <summary>
+            /// Проверяет наличие матерных слов в фразах деда и увеличивает количество фингалов за каждое найденное слово.
+            /// </summary>
+            /// <param name="swearWords">Список матерных слов для проверки.</param>
+            /// <returns>Количество фингалов, полученных дедом.</returns>
+            public int CheckSwearWords(params string[] swearWords)
+            {
+                int bruisesAdded = 0;
+
+                foreach (var phrase in Phrases)
+                {
+                    foreach (var swear in swearWords)
+                    {
+                        if (phrase.Contains(swear, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Bruises++;
+                            bruisesAdded++;
+                        }
+                    }
+                }
+
+                return bruisesAdded;
+            }
+        }
+        /// <summary>
+        /// Основной метод программы, который выполняет все упражнения:
+        /// 1. Выводит массив случайных чисел и меняет местами два выбранных числа.
+        /// 2. Вычисляет сумму, произведение и среднее арифметическое элементов массива.
+        /// 3. Запрашивает ввод числа и рисует его, если оно от 0 до 9, или завершает программу.
+        /// 4. Создает 5 дедов и проверяет их фразы на наличие матерных слов, увеличивая количество фингалов.
+        /// </summary>
+        /// <param name="args">Аргументы командной строки (не используются).</param>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("№1");
+            /*Вывести на экран массив из 20 случайных чисел. Ввести два числа из этого массива,
+которые нужно поменять местами. Вывести на экран получившийся массив.*/
+            Ex1();
+
+            Console.WriteLine("№2");
+            /*Написать метод, где в качества аргумента будет передан массив (ключевое слово
+params). Вывести сумму элементов массива (вернуть). Вывести (ref) произведение
+массива. Вывести (out) среднее арифметическое в массиве.*/
+            double product = 0;
+            double sum = Ex2(new double[] { 1, 2, 3, 4, 5 }, ref product, out double average);
+
+            Console.WriteLine($"Массив: [1, 2, 3, 4, 5]");
+            Console.WriteLine($"Сумма: {sum}");
+            Console.WriteLine($"Произведение: {product}");
+            Console.WriteLine($"Среднее: {average}");
+
+
+            Console.WriteLine("№3");
+            /*Пользователь вводит числа. Если числа от 0 до 9, то необходимо в консоли нарисовать
+изображение цифры в виде символа #.Если число больше 9 или меньше 0, то консоль
+должна окраситься в красный цвет на 3 секунды и вывести сообщение об ошибке. Если
+пользователь ввёл не цифру, то программа должна выпасть в исключение. Программа
+завершает работу, если пользователь введёт слово: exit или закрыть (оба варианта
+должны сработать) - консоль закроется.*/
+            Ex3();
+
+
+            Console.WriteLine("№4");
+            /*Создать структуру Дед. У деда есть имя, уровень ворчливости (перечисление), массив
+фраз для ворчания (прим.: “Проститутки!”, “Гады!”), количество синяков от ударов
+бабки = 0 по умолчанию. Создать 5 дедов. У каждого деда - разное количество фраз для
+ворчания. Напишите метод (внутри структуры), который на вход принимает деда,
+список матерных слов (params). Если дед содержит в своей лексике матерные слова из
+списка, то бабка ставит фингал за каждое слово. Вернуть количество фингалов.*/
+            Ded ded1 = new Ded("Иван", GrumpinessLevel.Low, new[] { "Проститутки!", "Лень кругом!" });
+            Ded ded2 = new Ded("Пётр", GrumpinessLevel.Medium, new[] { "Гады!", "Опять интернет включили!", "Жизнь плохая!", "Проститутки!" });
+            Ded ded3 = new Ded("Семен", GrumpinessLevel.High, new[] { "Пошляки!", "Бездельники!" });
+            Ded ded4 = new Ded("Григорий", GrumpinessLevel.Low, new[] { "Почему хлеб дорогой?" });
+            Ded ded5 = new Ded("Алексей", GrumpinessLevel.Medium, new[] { "Работы нет!", "Все уехали!" });
+
+            Ded[] dedArray = { ded1, ded2, ded3, ded4, ded5 };
+            string[] swearWords = { "Проститутки", "Гады", "Пошляки" };
+            foreach (var ded in dedArray)
+            {
+                int bruises = ded.CheckSwearWords(swearWords);
+                Console.WriteLine($"Дед {ded.Name} получил {bruises} фингал(а/ов). Всего фингалов: {ded.Bruises}");
+            }
+        }
+    }
+
+
+}
+
+
---
+++ Lab4/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        /// <summary>
+        /// Возвращает наибольшее из двух целых чисел.
+        /// </summary>
+        /// <param name="num1">Первое число.</param>
+        /// <param name="num2">Второе число.</param>
+        /// <returns>Наибольшее из двух чисел.</returns>
+
+        static int Maximum(int num1, int num2)
+        {
+            return Math.Max(num1, num2);
+        }
+
+        /// <summary>
+        /// Меняет местами значения двух передаваемых параметров.
+        /// </summary>
+        /// <typeparam name="T">Тип передаваемых параметров.</typeparam>
+        /// <param name="a">Первый параметр, передаваемый по ссылке.</param>
+        /// <param name="b">Второй параметр, передаваемый по ссылке.</param>
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// Вычисляет факториал числа с использованием выходного параметра.
+        /// </summary>
+        /// <param name="number">Число, для которого вычисляется факториал (должно быть >= 0).</param>
+        /// <param name="result">Результат вычисления факториала.</param>
+        /// <returns>Возвращает true, если вычисление успешно, иначе false при переполнении.</returns>
+        static bool TryCalculateFactorial(int number, out long result)
+        {
+            result = 1;
+
+            if (number < 0)
+            {
+                Console.WriteLine("Факториал определён только для неотрицательных чисел.");
+                return false;
+            }
+            try
+            {
+                checked
+                {
+                    for (int i = 1; i <= number; i++)
+                    {
+                        result *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                result = 0;
+                return false;
+            }
+        }
+        /// <summary>
+        /// Рекурсивно вычисляет факториал числа.
+        /// </summary>
+        /// <param name="number">Число, для которого вычисляется факториал.</param>
+        /// <returns>Факториал числа.</returns>
+        static long RecursiveFactorial(int number)
+        {
+            if (number < 0)
+                throw new ArgumentException("Факториал определён только для неотрицательных чисел.");
+            if (number == 0 || number == 1)
+                return 1;
+            return number * RecursiveFactorial(number - 1);
+        }
+        /// <summary>
+        /// Вычисляет НОД (наибольший общий делитель) двух чисел с использованием алгоритма Евклида.
+        /// </summary>
+        /// <param name="a">Первое число.</param>
+        /// <param name="b">Второе число.</param>
+        /// <returns>НОД двух чисел.</returns>
+
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        /// <summary>
+        /// Вычисляет НОД (наибольший общий делитель) трёх чисел с использованием алгоритма Евклида.
+        /// </summary>
+        /// <param name="a">Первое число.</param>
+        /// <param name="b">Второе число.</param>
+        /// <param name="c">Третье число.</param>
+        /// <returns>НОД трёх чисел.</returns>
+
+        static int GCD(int a, int b, int c)
+        {
+            return GCD(GCD(a, b), c);
+        }
+
+        /// <summary>
+        /// Рекурсивно вычисляет значение n-го числа ряда Фибоначчи.
+        /// </summary>
+        /// <param name="n">Номер числа в ряду Фибоначчи (должен быть положительным).</param>
+        /// <returns>Значение n-го числа ряда Фибоначчи.</returns>
+        /// <exception cref="ArgumentException">Выбрасывается, если номер числа меньше или равен 0.</exception>
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 0)
+                throw new ArgumentException("Порядковый номер должен быть положительным числом.");
+            if (n == 1 || n == 2)
+                return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 5.1");
+            /*
+            Написать метод, возвращающий наибольшее из двух чисел. Входные
+параметры метода – два целых числа. Протестировать метод.
+*/
+            Console.WriteLine($"Наибольшее значение из цифр 12 и 2 = {Maximum(12, 2)}");
+
+            Console.WriteLine("Упражнение 5.2");
+            /*
+Написать метод, который меняет местами значения двух передаваемых
+параметров. Параметры передавать по ссылке. Протестировать метод.
+            */
+            int num1 = 5, num2 = 10;
+            Console.WriteLine($"До Swap: num1 = {num1}, num2 = {num2}");
+            Swap(ref num1, ref num2);
+            Console.WriteLine($"После Swap: num1 = {num1}, num2 = {num2}");
+
+            Console.WriteLine("Упражнение 5.3");
+            /*
+            Написать метод вычисления факториала числа, результат вычислений
+передавать в выходном параметре. Если метод отработал успешно, то вернуть значение true;
+если в процессе вычисления возникло переполнение, то вернуть значение false. Для
+отслеживания переполнения значения использовать блок checked.
+            */
+            if (TryCalculateFactorial(20, out long factorial))
+            {
+                Console.WriteLine($"Факториал числа 20 равен {factorial}.");
+            }
+            else
+            {
+                Console.WriteLine("Произошло переполнение при вычислении факториала.");
+            }
+
+            Console.WriteLine("Упражнение 5.4");
+            /*
+Написать рекурсивный метод вычисления факториала числа.
+            */
+            Console.Write("Введите число для вычисления факториала рекурсивным методом: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine($"Факториал числа {number} равен {RecursiveFactorial(number)}.");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода числа");
+            }
+
+
+            Console.WriteLine("Домашнее задание 5.1");
+            /*
+Написать метод, который вычисляет НОД двух натуральных чисел
+(алгоритм Евклида). Написать метод с тем же именем, который вычисляет НОД трех
+натуральных чисел.
+            */
+            Console.Write("Введите три числа для вычисления НОД: ");
+            if (int.TryParse(Console.ReadLine(), out int number1) && int.TryParse(Console.ReadLine(), out int number2) && int.TryParse(Console.ReadLine(), out int number3))
+            {
+                Console.WriteLine($"НОД чисел({number1}, {number2}) = {GCD(number1, number2)}");
+                Console.WriteLine($"НОД({number1}, {number2}, {number3}) = {GCD(number1, number2, number3)}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода трёх чисел для вычисления НОД");
+            }
+
+            Console.WriteLine("Домашнее задание 5.2");
+            /*
+Написать рекурсивный метод, вычисляющий значение n-го числа
+ряда Фибоначчи. Ряд Фибоначчи – последовательность натуральных чисел 1, 1, 2, 3, 5, 8,
+13... Для таких чисел верно соотношение Fk = Fk-1 + Fk-2 .
+            */
+
+            Console.Write("Введите номер числа Фибоначчи: ");
+
+            if (int.TryParse(Console.ReadLine(), out int index))
+            {
+                Console.WriteLine($"Число Фибоначчи под номером {index} равно {Fibonacci(index)}.");
+
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода номерa числа Фибоначчи");
+            }
+        }
+
+
+
+
+    }
+}
+
